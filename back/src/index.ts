1f1784278f64ef16f5c8032493b03af64bde06d1
krcdb <memartin@student.42.fr>
2025-09-09 13:58:13 +0200

    chore: typo

--- back/src/index.ts
+++ back/src/index.ts
@@ -12,5 +12,5 @@ const httpServer = createServer(app);
 const io = MyWebSocket.getInstance(httpServer);
 
 httpServer.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
